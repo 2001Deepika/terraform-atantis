3627bd3463b403358508cf6aa6b1e506a436ff37
2001Deepika <126582907+2001Deepika@users.noreply.github.com>
2025-03-24 20:19:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo 'Testing Atlantis setup!'"
+provider "aws"{
+    region ="ap-south-1"
+}
+terraform {
+  backend "s3" {
+    bucket = "mmyyyy"
+    key="atlantis/terraform.tfstate"
+    region = "ap-south-1"
+    profile = "default"
   }
 }
-sudo apt install terraform
+resource "aws_instance" "this" {
+  ami="ami-0e35ddab05955cf57"
+  instance_type = "t2.micro"
+  tags = {
+    Name="terrafom-atlantis-demo"
+  }
+}
